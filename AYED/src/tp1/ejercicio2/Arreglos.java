379617967c7ebfb8a768a9b99c53a89a34a2287d
Francisco Aramayo <aramayo.francisco04@gmail.com>
2025-09-17 13:34:07 -0300

    prueba

--- AYED/src/tp1/ejercicio2/Arreglos.java
+++ AYED/src/tp1/ejercicio2/Arreglos.java
@@ -8,6 +8,7 @@ public class Arreglos {
 		for (int i = 0 ; i < n ; i++) {
 			arregloM[i] = (i+1) * n;
 		}
+		System.out.println("");
 		return arregloM;
 		
 	}
---
+++ AYED/src/tp1/ejercicio2/Hola.java
@@ -0,0 +1,7 @@
+package tp1.ejercicio2;
+
+public class Hola {
+	public static void main (String [] args) {
+		System.out.println("Hola!!!");
+	}
+}
